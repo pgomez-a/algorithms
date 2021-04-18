ea9af69594b4f1c77f6bdbe18edb0d5e3b8353bd
Pablo Gomez alvarez <pgomez-a@c1r14s6.42madrid.com>
2021-04-18 12:24:49 +0200

    commit 1.2

--- algorithm.h
+++ algorithm.h
@@ -1,9 +1,7 @@
 #ifndef ALGORITHM_H
 # define ALGORITHM_H
 
-# include <stdio.h>
-
-int	binary_search(int num, int len, int *list);
+int		binary_search(int num, int len, int *list);
 void	selection_sort(int len, int *list);
 
-# endif
+#endif
